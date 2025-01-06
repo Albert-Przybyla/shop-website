0262b3ac75a77481fc948afbc8c9a04f1fe88a9f
APrzybyla <106275293+Albert-Przybyla@users.noreply.github.com>
2025-01-06 13:28:11 +0100

    prod fixes

--- user-web/api/api.ts
+++ user-web/api/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: `http://localhost:9005`,
+  baseURL: `http://185.201.112.115:8081`,
   timeout: 5000,
   headers: {
     "Content-Type": "application/json",
