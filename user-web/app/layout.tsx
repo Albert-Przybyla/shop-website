dc041cf4b3947ad9fe115cf67fecd7e75746bf1b
APrzybyla <106275293+Albert-Przybyla@users.noreply.github.com>
2025-01-09 20:42:19 +0100

    fix

--- user-web/app/layout.tsx
+++ user-web/app/layout.tsx
@@ -5,6 +5,7 @@ import Head from "next/head";
 import "./globals.css";
 import FloatingButton from "@/components/FloatingButton";
 import { Bounce, ToastContainer } from "react-toastify";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "Blue Elephant",
@@ -57,9 +58,37 @@ export default function RootLayout({
   return (
     <html lang="en">
       <Head>
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-NHM7NMB8');
+          `}
+        </Script>
         <link rel="icon" href="/images/logo-max.png" />
       </Head>
       <body className={`antialiased`}>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NHM7NMB8"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        <Script src="https://www.googletagmanager.com/gtag/js?id=G-GTM-NHM7NMB8" strategy="afterInteractive" />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-XXXXXXXXXX', {
+              page_path: window.location.pathname,
+            });
+          `}
+        </Script>
         <Nav />
         <ToastContainer
           position="top-right"
--- user-web/app/page.tsx
+++ user-web/app/page.tsx
@@ -1,12 +1,10 @@
 import Products from "@/components/Products";
 import Slider from "@/components/Slider";
-import { GoogleAnalytics } from "nextjs-google-analytics";
 export const revalidate = 60;
 
 export default function Home() {
   return (
     <>
-      <GoogleAnalytics trackPageViews />
       <div className="border-b">
         <Slider />
       </div>
