43c21480359079c249c06ebc07c9ef98db13cc7c
APrzybyla <106275293+Albert-Przybyla@users.noreply.github.com>
2025-01-09 20:09:59 +0100

    Google Analytics

--- user-web/app/page.tsx
+++ user-web/app/page.tsx
@@ -1,11 +1,12 @@
 import Products from "@/components/Products";
 import Slider from "@/components/Slider";
-
+import { GoogleAnalytics } from "nextjs-google-analytics";
 export const revalidate = 60;
 
 export default function Home() {
   return (
     <>
+      <GoogleAnalytics trackPageViews />
       <div className="border-b">
         <Slider />
       </div>
